96656d81932880e990b6becb4421e058d6d86e4f
N7732 <oliviernshimyumuremyi4@gmail.com>
2025-11-30 22:37:51 +0200

    First OOP Pilar Classes and Objects Exercises 1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using name;
+
+namespace name
+{
+
+    class Car
+    {
+        public string Names { get; set; }="";
+        public int Year;
+        public string Owner{get; set; }="";
+
+        public void Display()
+        {
+            Console.WriteLine($"Name:{Names}\nYear:{Year}\nOwner:{Owner}\n\nThanks!");
+        }
+    }
+
+    class Cars
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Names = "Volvo";
+            car.Year = 2019;
+            car.Owner = "Olivier";
+
+            car.Display();
+        }
+    }
+}
